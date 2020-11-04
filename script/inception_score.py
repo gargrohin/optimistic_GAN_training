b1d5a85d6d74e5481fdae034f9c277e6cd370951
Rohin Garg <gargrohin0@gmail.com>
2020-11-04 17:45:06 +0530

    eval IS

--- script/inception_score.py
+++ script/inception_score.py
@@ -15,6 +15,7 @@ import glob
 import scipy.misc
 import math
 import sys
+from tqdm import tqdm, trange
 
 MODEL_DIR = '/tmp/imagenet'
 DATA_URL = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
@@ -26,7 +27,7 @@ def get_inception_score(images, splits=10):
   assert(type(images) == list)
   assert(type(images[0]) == np.ndarray)
   assert(len(images[0].shape) == 3)
-  #assert(np.max(images[0]) > 10)
+  assert(np.max(images[0]) > 10)
   assert(np.min(images[0]) >= 0.0)
   inps = []
   for img in images:
@@ -34,15 +35,14 @@ def get_inception_score(images, splits=10):
     inps.append(np.expand_dims(img, 0))
   bs = 100
   with tf.Session() as sess:
-    preds = []
+    preds, pools = [], []
     n_batches = int(math.ceil(float(len(inps)) / float(bs)))
-    for i in range(n_batches):
-        # sys.stdout.write(".")
-        # sys.stdout.flush()
-        inp = inps[(i * bs):min((i + 1) * bs, len(inps))]
-        inp = np.concatenate(inp, 0)
-        pred = sess.run(softmax, {'ExpandDims:0': inp})
-        preds.append(pred)
+    for i in trange(n_batches):
+      inp = inps[(i * bs):min((i + 1) * bs, len(inps))]
+      inp = np.concatenate(inp, 0)
+      pred, pool = sess.run([softmax, pool3], {'InputTensor:0': inp})
+      preds.append(pred)
+      pools.append(pool)
     preds = np.concatenate(preds, 0)
     scores = []
     for i in range(splits):
@@ -50,11 +50,12 @@ def get_inception_score(images, splits=10):
       kl = part * (np.log(part) - np.log(np.expand_dims(np.mean(part, 0), 0)))
       kl = np.mean(np.sum(kl, 1))
       scores.append(np.exp(kl))
-    return np.mean(scores), np.std(scores)
+    return np.mean(scores), np.std(scores), np.squeeze(np.concatenate(pools, 0))
 
 # This function is called automatically.
+# Init inception
 def _init_inception():
-  global softmax
+  global softmax, pool3
   if not os.path.exists(MODEL_DIR):
     os.makedirs(MODEL_DIR)
   filename = DATA_URL.split('/')[-1]
@@ -73,25 +74,30 @@ def _init_inception():
       MODEL_DIR, 'classify_image_graph_def.pb'), 'rb') as f:
     graph_def = tf.GraphDef()
     graph_def.ParseFromString(f.read())
-    _ = tf.import_graph_def(graph_def, name='')
+    input_tensor = tf.placeholder(tf.float32, shape=[None, None, None, 3],
+                                  name='InputTensor')
+    _ = tf.import_graph_def(graph_def, name='',
+                            input_map={'ExpandDims:0':input_tensor})
+    # _ = tf.import_graph_def(graph_def, name='')
   # Works with an arbitrary minibatch size.
   with tf.Session() as sess:
     pool3 = sess.graph.get_tensor_by_name('pool_3:0')
     ops = pool3.graph.get_operations()
     for op_idx, op in enumerate(ops):
-        for o in op.outputs:
-            shape = o.get_shape()
-            shape = [s.value for s in shape]
-            new_shape = []
-            for j, s in enumerate(shape):
-                if s == 1 and j == 0:
-                    new_shape.append(None)
-                else:
-                    new_shape.append(s)
-            o._shape = tf.TensorShape(new_shape)
+      for o in op.outputs:
+        shape = o.get_shape()
+        shape = [s.value for s in shape]
+        new_shape = []
+        for j, s in enumerate(shape):
+          if s == 1 and j == 0:
+            new_shape.append(None)
+          else:
+            new_shape.append(s)
+        o.set_shape(tf.TensorShape(new_shape))
     w = sess.graph.get_operation_by_name("softmax/logits/MatMul").inputs[1]
-    logits = tf.matmul(tf.squeeze(pool3), w)
+    logits = tf.matmul(tf.squeeze(pool3, [1,2]), w)
     softmax = tf.nn.softmax(logits)
+
 
 if softmax is None:
   _init_inception()

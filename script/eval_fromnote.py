1e0c008586f274326c25bdb07f6130bbff1ddef3
Rohin Garg <gargrohin0@gmail.com>
2020-11-04 17:39:48 +0530

    eval IS

---
+++ script/eval_fromnote.py
@@ -0,0 +1,44 @@
+from os.path import join,exists,realpath,dirname,basename
+from os import makedirs,listdir, system
+import numpy as np, cPickle, editdistance, seaborn as sns
+import matplotlib.pyplot as plt, pandas as pd, itertools, glob, h5py
+from scipy.stats import entropy
+from matplotlib.font_manager import FontProperties
+from IPython.display import display
+from collections import defaultdict
+from IPython.display import display
+from itertools import izip
+from scipy.stats import ranksums
+import multiprocessing as mp
+from PIL import Image
+
+import inception_score
+
+rundir = 'cifar10/'
+e = 100
+
+def get_score(improved_keras_dir, t_n_epoch):
+    score = []
+    for i in range(t_n_epoch-10, t_n_epoch):
+        scorefile = join(improved_keras_dir, 'epoch_{}.score'.format(i))
+        if not exists(scorefile):   
+            datafile = join(improved_keras_dir, 'epoch_{}.pkl'.format(i))
+            if not exists(datafile):
+                break
+            with open(datafile) as f:
+                sample = cPickle.load(f)
+                t_score = inception_score.get_inception_score(list(sample), 1)[0]
+            with open(scorefile, 'w') as f:
+                f.write('%f\n' % t_score)
+        else:
+            with open(scorefile) as f:
+                t_score = float(f.readline())
+        score.append(t_score)
+    
+    return max(score)
+
+expt2plot = ['adam_ratio1', 'optimAdam', 'optimAdam_ratio1']
+
+for expt in expt2plot:
+    score = get_score(join(rundir, expt), e)
+    print(expt, score)
